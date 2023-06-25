d30edad24a838382b66b4cb6f07815a8606a03eb
Kevin Stark <kstark734@gmail.com>
2023-06-25 22:27:09 +0100

    Create Paragraph component

---
+++ src/components/Paragraph.tsx
@@ -0,0 +1,5 @@
+import {Typography} from '@mui/material';
+
+export default function Paragraph(content: string) {
+  return <Typography variant="body1">{content}</Typography>;
+}
--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -3,14 +3,15 @@ import type {GetServerSideProps, InferGetServerSidePropsType} from 'next';
 import {Container, Typography} from '@mui/material';
 
 type PostType = {};
-export const getServersideProps: GetServerSideProps<{postData: PostType}> = async context => {
+export const getServerSideProps: GetServerSideProps<{postData: PostType}> = async context => {
   const {id} = context.params as {id: string};
   const res = await fetch(`https://payload-cms-test-production.up.railway.app/api/posts/${id}`);
   const postData = await res.json();
   return {props: {postData}};
 };
 
-export default function Post() {
+export default function Post({postData}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(postData);
   return (
     <Container
       sx={{
