ae8c48561f974d9eab8d801ff71aaf8b1152e33a
Kevin Stark <kstark734@gmail.com>
2023-07-05 19:48:42 +0100

    setup redux slice and redux page

---
+++ src/pages/redux.tsx
@@ -0,0 +1,9 @@
+import {Container, Typography} from '@mui/material';
+
+export default function Redux() {
+  return (
+    <Container sx={{paddingTop: 10, paddingBottom: 10, backgroundColor: 'lightskyblue'}}>
+      <Typography variant="h1">Redux Example</Typography>
+    </Container>
+  );
+}
--- src/redux/postSlice.ts
+++ src/redux/postSlice.ts
@@ -1,19 +1,55 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction, createAsyncThunk} from '@reduxjs/toolkit';
 import type {RootState} from '@/redux/store/store';
 
-export interface PostState {
-  posts: [] | null;
-}
+export type PostData = {
+  docs: [];
+  totalDocs: number;
+  totalPages: number;
+  page: number;
+  hasPrevPage: boolean;
+  hasNextPage: boolean;
+  prevPage: boolean | null;
+  nextPage: boolean | null;
+};
+
+export type PostState = {
+  data: PostData[];
+  loading: boolean;
+  error: boolean;
+};
 
 const initialState: PostState = {
-  posts: null,
+  data: [],
+  loading: false,
+  error: false,
 };
+
+export const getPosts = createAsyncThunk('posts/getPosts', async () => {
+  const response = await fetch('https://payload-cms-test-production.up.railway.app/api/posts');
+  const data = await response.json();
+  return data;
+});
 
 export const postSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {},
+  reducers: {
+    rehydrate(state, action: PayloadAction<PostState>) {
+      // @ts-ignore
+      state.data = action.payload;
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(getPosts.pending, state => {
+        state.loading = true;
+      })
+      .addCase(getPosts.fulfilled, (state, {payload}) => {
+        state.loading = false;
+        state.data = payload;
+      });
+  },
 });
 
-export const selectPosts = (state: RootState) => state.posts.posts;
+export const selectPosts = (state: RootState) => state.posts.data;
 export default postSlice.reducer;
