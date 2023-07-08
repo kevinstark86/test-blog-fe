b7db10d1d83d71bf6acbf892ebc85ce4958d1e0a
Kevin Stark <kstark734@gmail.com>
2023-07-08 19:58:08 +0100

    Add getStaticProps

--- src/pages/static.tsx
+++ src/pages/static.tsx
@@ -1,6 +1,18 @@
+import type {InferGetStaticPropsType, GetStaticProps} from 'next';
 import {Box, Container, Typography} from '@mui/material';
 
-export default function Static() {
+type BlogData = {
+  docs: [];
+};
+
+export const getStaticProps: GetStaticProps<{data: BlogData}> = async () => {
+  const res = await fetch('https://payload-cms-test-production.up.railway.app/api/posts');
+  const data = await res.json();
+  return {props: {data}};
+};
+
+export default function Static({data}: InferGetStaticPropsType<typeof getStaticProps>) {
+  console.log('here are static post data', data);
   return (
     <Container sx={{paddingTop: 10, paddingBottom: 10, backgroundColor: 'lightskyblue'}}>
       <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
