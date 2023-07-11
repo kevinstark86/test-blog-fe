3dc7251ddd7d33e0e4d7bb475885accfa77ef394
Kevin Stark <kstark734@gmail.com>
2023-07-11 22:15:02 +0100

    Type query

--- src/pages/pagination.tsx
+++ src/pages/pagination.tsx
@@ -1,10 +1,25 @@
-import {Box, Container, Typography} from '@mui/material';
+import {Box, Container, Typography, CircularProgress} from '@mui/material';
+import {useGetAllPostsQuery} from '@/redux/services/postAPi';
 
 export default function Pagination() {
+  const {data, error, isLoading} = useGetAllPostsQuery();
   return (
     <Container sx={{paddingTop: 10, paddingBottom: 10, backgroundColor: 'lightskyblue'}}>
       <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-        <Typography variant="h1">Redux Example</Typography>
+        <Typography variant="h1">Pagination With RTKQ</Typography>
+      </Box>
+      <Box>
+        {/* eslint-disable-next-line no-nested-ternary */}
+        {error ? (
+          <Typography variant="body1" color="red">
+            There has been an error
+          </Typography>
+        ) : // eslint-disable-next-line no-nested-ternary
+        isLoading ? (
+          <CircularProgress />
+        ) : data ? (
+          <Typography variant="h4">Check console logs</Typography>
+        ) : null}
       </Box>
     </Container>
   );
--- src/redux/services/postAPi.ts
+++ src/redux/services/postAPi.ts
@@ -1,12 +1,16 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
+type Posts = {
+  docs: [];
+};
+
 export const blogApi = createApi({
   reducerPath: 'blogApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://payload-cms-test-production.up.railway.app/api/',
   }),
   endpoints: builder => ({
-    getAllPosts: builder.query({
+    getAllPosts: builder.query<Posts, void>({
       query: () => `posts`,
     }),
   }),
