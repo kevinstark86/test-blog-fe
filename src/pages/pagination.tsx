e0242c6a27b706ff84ca751a4b79c0cea71b18f5
Kevin Stark <kstark734@gmail.com>
2023-07-12 15:43:05 +0100

    map over data from rtkq and return cards

--- src/pages/pagination.tsx
+++ src/pages/pagination.tsx
@@ -1,8 +1,12 @@
-import {Box, Container, Typography, CircularProgress} from '@mui/material';
+import {Box, Container, Typography, CircularProgress, Grid} from '@mui/material';
 import {useGetAllPostsQuery} from '@/redux/services/postAPi';
+import SimpleCard from '@/components/Card';
 
 export default function Pagination() {
   const {data, error, isLoading} = useGetAllPostsQuery();
+  if (data) {
+    console.log('query data response', data);
+  }
   return (
     <Container sx={{paddingTop: 10, paddingBottom: 10, backgroundColor: 'lightskyblue'}}>
       <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
@@ -18,7 +22,28 @@ export default function Pagination() {
         isLoading ? (
           <CircularProgress />
         ) : data ? (
-          <Typography variant="h4">Check console logs</Typography>
+          <Grid container spacing={2}>
+            {data.docs.map(post => {
+              const {
+                title,
+                id,
+                featuredImage: {url},
+                publishedDate,
+                author,
+                content,
+              } = post;
+              return (
+                <SimpleCard
+                  id={id}
+                  title={title}
+                  img={url}
+                  publishedDate={publishedDate}
+                  author={author}
+                  content={content}
+                />
+              );
+            })}
+          </Grid>
         ) : null}
       </Box>
     </Container>
